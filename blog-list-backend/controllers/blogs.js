40fde30feb54143f0b6cc1ecdb09a91d00388f2a
Paweł Klimczuk <pklimczu@gmail.com>
2020-11-24 23:17:08 +0100

    Finished chapter 4b Testing the backend

--- blog-list-backend/controllers/blogs.js
+++ blog-list-backend/controllers/blogs.js
@@ -14,4 +14,21 @@ blogsRouter.post('/', async (request, response) => {
     return response.status(201).json(result)
 })
 
+blogsRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndRemove(request.params.id)
+    return response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+    const body = request.body
+
+    const blog = {
+        ...body,
+        likes: body.likes
+    }
+
+    const updatedNote = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    return response.status(200).json(updatedNote)
+})
+
 module.exports = blogsRouter
--- blog-list-backend/tests/blog.test.js
+++ blog-list-backend/tests/blog.test.js
@@ -17,10 +17,116 @@ beforeEach(async () => {
 
 })
 
-test('all blogs are returned', async () => {
-    const resposne = await api.get('/api/blogs')
+describe('get tests', () => {
+    test('all blogs are returned', async () => {
+        const response = await api.get('/api/blogs')
 
-    expect(resposne.body).toHaveLength(initialBlogs.length)
+        expect(response.body).toHaveLength(initialBlogs.length)
+    })
+
+    test('returned blog has id property', async () => {
+        const response = await api.get('/api/blogs')
+
+        expect(response.body[0].id).toBeDefined()
+    })
+
+    test('blogs are correctly added', async () => {
+        const newEntry = {
+            title: 'Test title',
+            author: 'Romuald',
+            url: 'https://sptth.com',
+            likes: 3
+        }
+
+        await api
+            .post('/api/blogs')
+            .send(newEntry)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const response = await api.get('/api/blogs')
+
+        const title = response.body.map(r => r.title)
+
+        expect(response.body).toHaveLength(initialBlogs.length + 1)
+        expect(title).toContain(
+            newEntry.title
+        )
+    })
+})
+
+describe('post tests', () => {
+    test('if likes property is missing, 0 is assigned', async () => {
+        const newEntry = {
+            title: 'Test title',
+            author: 'Romuald',
+            url: 'https://sptth.com',
+        }
+
+        await api
+            .post('/api/blogs')
+            .send(newEntry)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const response = await api.get('/api/blogs')
+
+        const likes = response.body.map(r => r.likes)
+
+        expect(response.body).toHaveLength(initialBlogs.length + 1)
+        expect(likes[2]).toBe(0)
+    })
+
+    test('if title and url properties are missing, 400 is returned', async () => {
+        const newEntry = {
+            url: 'https://sptth.com'
+        }
+
+        await api
+            .post('/api/blogs')
+            .send(newEntry)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+    })
+})
+
+describe('delete method', () => {
+    test('entry is deleted', async () => {
+        const entries = await helper.blogsInDb()
+        const blogToRemove = entries[0]
+
+        await api
+            .delete(`/api/blogs/${blogToRemove.id}`)
+            .expect(204)
+
+        const response = await helper.blogsInDb()
+
+        expect(response).toHaveLength(initialBlogs.length - 1)
+    })
+})
+
+describe('put method', () => {
+    test('likes are updated', async () => {
+        const entries = await helper.blogsInDb()
+        const blogToUpdate = entries[0]
+
+        const newEntry = {
+            ...blogToUpdate,
+            likes: 100
+        }
+
+        await api
+            .put(`/api/blogs/${newEntry.id}`)
+            .send(newEntry)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        const response = await helper.blogsInDb()
+
+        const likes = response.map(r => r.likes)
+
+        expect(likes[0]).toBe(100)
+    })
 })
 
 afterAll(() => {
